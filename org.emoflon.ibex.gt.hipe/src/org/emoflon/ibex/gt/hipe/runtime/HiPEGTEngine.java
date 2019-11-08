07dfee9587fa568c2a922cf9f0e6aadfaac2ea49
arg0n1s <basti_seppl@yahoo.com>
2019-11-08 18:30:53 +0100

    # required change to make the auto apply fix work

--- org.emoflon.ibex.gt.hipe/src/org/emoflon/ibex/gt/hipe/runtime/HiPEGTEngine.java
+++ org.emoflon.ibex.gt.hipe/src/org/emoflon/ibex/gt/hipe/runtime/HiPEGTEngine.java
@@ -294,6 +294,7 @@ public class HiPEGTEngine implements IContextPatternInterpreter {
 			Map<String, ProductionResult> extractData = engine.extractData();
 			addNewMatches(extractData);
 			deleteInvalidMatches(extractData);
+			app.notifySubscriptions();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
