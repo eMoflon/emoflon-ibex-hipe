f13f732a556186b16e0342daccd3933d4172563f
Arikae <arikae00@jesitec.de>
2020-04-28 09:35:17 +0200

    fixed bug where the reg helper was generated into the run folder

--- org.emoflon.ibex.tgg.ide.hipe/src/org/emoflon/ibex/tgg/compiler/hipe/defaults/IbexHiPEBuilderExtension.java
+++ org.emoflon.ibex.tgg.ide.hipe/src/org/emoflon/ibex/tgg/compiler/hipe/defaults/IbexHiPEBuilderExtension.java
@@ -261,7 +261,7 @@ public class IbexHiPEBuilderExtension implements BuilderExtension {
 		String input_srcPackage = srcPkg == null ? "<<SRC_Package>>" : srcPkg;
 		String input_trgPackage = trgPkg == null ? "<<TRG_Package>>" : trgPkg;
 		
-		builder.createDefaultRunFile(HiPEFilesGenerator.REGISTRATION_HELPER, (projectName, fileName)
+		builder.createDefaultConfigFile(HiPEFilesGenerator.REGISTRATION_HELPER, (projectName, fileName)
 				-> HiPEFilesGenerator.generateRegHelperFile(projectName, input_srcProject, input_trgProject, input_srcPackage, input_trgPackage));
 	}
 	
