f3c402936edbe38da1e4485dca68dbce6f1ee9ea
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2020-07-14 22:18:11 +0200

    Sort TGGMatch parameter

--- org.emoflon.ibex.gt.hipe/src/org/emoflon/ibex/gt/hipe/runtime/HiPEGTMatch.java
+++ org.emoflon.ibex.gt.hipe/src/org/emoflon/ibex/gt/hipe/runtime/HiPEGTMatch.java
@@ -1,25 +1,37 @@
 package org.emoflon.ibex.gt.hipe.runtime;
 
+import java.util.List;
+
 import org.emoflon.ibex.common.operational.SimpleMatch;
+import org.emoflon.ibex.tgg.compiler.patterns.PatternSuffixes;
+import org.emoflon.ibex.tgg.operational.matches.TGGMatchParameterOrderProvider;
+
 import hipe.engine.match.ProductionMatch;
 
-
 /**
- * A graph transformation match from Democles.
+ * A graph transformation match from HiPE.
  */
 public class HiPEGTMatch extends SimpleMatch {
 	/**
 	 * Creates a new HiPEGTMatch with the given match and pattern.
 	 * 
-	 * @param match
-	 *            the HiPE match
-	 * @param pattern
-	 *            the HiPE pattern
+	 * @param match   the HiPE match
+	 * @param pattern the HiPE pattern
 	 */
 	public HiPEGTMatch(final ProductionMatch match, String patternName) {
 		super(patternName);
-		for(String label : match.getLabels()) {
-			put(label, match.getNode(label));
+		List<String> params = null;
+		if (patternName != null)
+			params = TGGMatchParameterOrderProvider.getParams(PatternSuffixes.removeSuffix(patternName));
+		if (params != null) {
+			for (String p : params) {
+				if (match.getLabels().contains(p))
+					put(p, match.getNode(p));
+			}
+		} else {
+			for (String label : match.getLabels()) {
+				put(label, match.getNode(label));
+			}
 		}
 	}
 }
