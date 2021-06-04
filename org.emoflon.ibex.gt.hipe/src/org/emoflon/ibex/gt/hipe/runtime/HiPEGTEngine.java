b2c135ad20343099530551ebdd4b9807515ebaf4
isabella-grieser <isabella.nunesgrieser@stud.tu-darmstadt.de>
2021-06-04 06:39:34 +0200

    now works with the XtendResource

--- org.emoflon.ibex.gt.hipe/src/org/emoflon/ibex/gt/hipe/runtime/HiPEGTEngine.java
+++ org.emoflon.ibex.gt.hipe/src/org/emoflon/ibex/gt/hipe/runtime/HiPEGTEngine.java
@@ -43,6 +43,7 @@ import hipe.engine.IHiPEEngine;
 import hipe.engine.match.ProductionMatch;
 import hipe.engine.message.production.ProductionResult;
 import hipe.network.HiPENetwork;
+import persistence.XtendXMIResourceFactoryImpl;
 
 /**
  * Engine for (unidirectional) graph transformations with HiPE.
@@ -122,7 +123,7 @@ public class HiPEGTEngine implements IContextPatternInterpreter {
 	private ResourceSet createAndPrepareResourceSet_internal(final String workspacePath) {
 		ResourceSet rs = new ResourceSetImpl();
 		rs.getResourceFactoryRegistry().getExtensionToFactoryMap()
-				.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XtendXMIResourceFactoryImpl());
 		try {
 			rs.getURIConverter().getURIMap().put(URI.createPlatformResourceURI("/", true), URI.createFileURI(new File(workspacePath).getCanonicalPath() + File.separator));
 		} catch (IOException e) {
@@ -189,8 +190,8 @@ public class HiPEGTEngine implements IContextPatternInterpreter {
 	}
 	
 	protected Resource loadResource(String path) throws Exception {
-		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XtendXMIResourceFactoryImpl());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi",new XtendXMIResourceFactoryImpl());
 		
 		Resource modelResource = resourceSet.getResource(URI.createURI(path).resolve(base), true);
 		EcoreUtil.resolveAll(resourceSet);
